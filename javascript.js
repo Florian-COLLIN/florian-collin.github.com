0884da7ed67b209c3fbb2aafcf5d7068743bfc8e
Florian-COLLIN <73430474+Florian-COLLIN@users.noreply.github.com>
2021-02-16 20:53:03 +0100

    Update

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
 /* @author : Florian COLLIN
  */
-var donnees = {
+/* var donnees = {
 	author: "Florian COLLIN",
 	date: "",
 	update: "1er janvier 2021",
 	version: "1.1.4"
 };
+*/
+function annee() {
+	var anneeEnCours = new Date();
+	window.document.write(anneeEnCours.getFullYear());
+}
