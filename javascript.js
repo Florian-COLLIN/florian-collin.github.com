17e37cd58d8f1847458a14ccb3a93e56aba03375
Florian COLLIN <florian.collin83@gmail.com>
2021-12-12 19:23:07 +0100

    Update

--- javascript.js
+++ javascript.js
@@ -16,3 +16,10 @@ function version() {
 	var version = "v1.2.0";
 	window.document.write(version);
 }
+/* Store */
+var ifsd_search = document.getElementById('ifsd_search');
+var link_edge = "";
+
+ifsd_search.onclick = function(){
+	window.document.write("<ul><li><a href=\""+link_edge+"\">Microsoft Edge</a></li></ul>")
+}
